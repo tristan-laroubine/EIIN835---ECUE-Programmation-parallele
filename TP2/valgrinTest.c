2c106e595029488d9faec1b22ea043a077aa1630
tristan-laroubine <laroubine.tristan@yahoo.fr>
2021-03-10 09:44:13 +0100

    fix

--- TP2/valgrinTest.c
+++ TP2/valgrinTest.c
@@ -2,9 +2,11 @@
 // Created by trist on 10/03/2021.
 //
 #include <stdlib.h>
+#include <stdio.h>
 void loopTablo(int *a, int size){
     for (int i = 0; i < size; ++i) {
         *(a+i)  = 1;
+        printf("%e | ",*(a+i));
     }
 }
 
